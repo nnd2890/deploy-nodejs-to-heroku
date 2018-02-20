fff14f63b4d1ff7183d1c0745ab96c48fa7d41e8
Ngoc Dai <dainnd00258@fpt.edu.vn>
2018-02-20 16:36:17 +0700

    sign in

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,7 @@ passport.use('local.signup', new LocalStrategy({
         passReqToCallback: true
     }, function(req, email, password, done) {
         req.checkBody('email', 'Invalid email').notEmpty().isEmail();
-        req.checkBody('pasword', 'Invalid pasword').notEmpty().isLength({min:4});
+        req.checkBody('password', 'Invalid password').notEmpty().isLength({min: 4});
         var errors = req.validationErrors();
         if (errors) {
             var messages = [];
@@ -50,3 +50,33 @@ passport.use('local.signup', new LocalStrategy({
         });
     }
 ));
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function(req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function(error) {
+            messages.push(error.msg)
+        });
+        console.log(messages);
+        return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({'email': email}, function(err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, {message: 'No User found.'});
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, {message: 'Wrong password.'})
+        }
+        return done(null, user);
+    });
+}));
