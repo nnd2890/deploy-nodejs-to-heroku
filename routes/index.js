9cb9d6ddb2d42fc1282a120b06d7bb1a0dbd52ed
Ngoc Dai <dainnd00258@fpt.edu.vn>
2018-02-21 16:55:31 +0700

    storing orders

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,72 @@
 var express = require('express');
 var router = express.Router(); 
 
-var Product = require('../models/product'); 
+var Product = require('../models/product');
+var Cart = require('../models/cart');
+var Order = require('../models/order');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash('success')[0];
   Product.find(function(err, docs) {
     var productChunks = [];
     var chunkSize = 3;
     for (var i = 0; i < docs.length; i += chunkSize) {
       productChunks.push(docs.slice(i, i + chunkSize));
     }
-    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks, successMsg: successMsg, noMessages: !successMsg});
+  });
+});
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
+
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      return res.redirect('/');
+    }
+    
+    cart.add(product, productId);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
+  });
+});
+
+router.get('/shopping-cart', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart', {product: null});
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
+router.get('/checkout', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  var order = new Order({
+    user: req.user,
+    cart: cart,
+    address: req.body.address,
+    name: req.body.name
+  });
+  order.save(function(err, result) {
+    req.flash('success', 'Successfully bought product!');
+    req.session.cart = null;
+    res.redirect('/');
   });
 });
 
