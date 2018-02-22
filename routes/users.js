a33e3fc8573cb75745a19da8a718d83b4336b385
Ngoc Dai <dainnd00258@fpt.edu.vn>
2018-02-22 08:51:25 +0700

    display orders

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,24 @@ var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
 
+var Order = require('../models/order');
+var Cart = require('../models/cart');
+
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
 router.get('/profile', isLoggedIn, function(req, res, next) {
-  res.render('user/profile');
+  Order.find({user: req.user}, function(err, orders) {
+    if (err) {
+      return res.write('Error');
+    }
+    var cart;
+    orders.forEach(function(order) {
+      cart = new Cart(order.cart);
+      order.items = cart.generateArray();
+    });
+    res.render('user/profile', {orders: orders});
+  });
 });
 
 router.get('/logout', isLoggedIn, function(req, res, next) {
@@ -30,6 +43,7 @@ router.post('/signup', passport.authenticate('local.signup', {
 }), function(req, res, next) {
   if (req.session.oldUrl) {
     var oldUrl = req.session.oldUrl;
+    console.log(oldUrl)
     req.session.oldUrl = null;
     res.redirect(oldUrl);
   } else {
