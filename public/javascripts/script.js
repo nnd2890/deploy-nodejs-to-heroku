c220e84b08962aa14846f5e4f231ea8529d2bdb7
Ngoc Dai <dainnd00258@fpt.edu.vn>
2018-03-21 18:34:20 +0700

    change shop-cart

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -45,30 +45,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-$(document).ready(function() {
-  alert(1);
-  $( ".delete-product" ).on( "click", function(e) {
-    if(confirm("Are you sure you want to delete this?")){
-      $target = $(e.target);
-      const id = $target.attr('data-id');
-      const csrfToken = $( "#_csrf" ).val();
-      $.ajax({
-        type: 'DELETE',
-        url: '/delete-product/'+id,
-        headers: {
-          'CSRF-Token': csrfToken 
-        },
-        success: function(response) {
-          location.reload();
-        },
-        error: function(err) {
-          console.log(err);
-        }
-      });
-    }
-    else{
-        return false;
-    }
-  });
-});
